f82988cc7f00d028e6df8ba87c344f6c4c65307d
unknown <d.loedermann@googlemail.com>
2025-08-10 18:38:23 +0200

    first php page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PhP for dummasses</title>
+</head>
+<body>
+    <h1>hallo welt</h1>
+    <?php
+    echo "<h1>Hallo PHP!</h1>"?>
+</body>
+</html>
