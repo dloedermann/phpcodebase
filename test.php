6540470c5a61c7ba036d8eb93f2326f6405a58b9
unknown <d.loedermann@googlemail.com>
2025-08-18 20:11:48 +0200

    php combine function for arrays

--- test.php
+++ test.php
@@ -1,3 +1,18 @@
 <h2>Whaaaatz uuuuuup?</h2>
 <?php
-return "Hallo aus der test.php Datei! Ich bin eine Rückgabe";
+
+$arr1 = [1,2,4,6,7];
+$arr2 = ["Hallo Datei! Ich bin PHP!", "Pedeer"];
+
+function add(...$numbers) {
+    for($i = 0; $i < count($numbers); $i++) {
+        echo $numbers[$i];
+    }
+}
+add(1,2,3,4,10);
+
+$combined = [...$arr1, ...$arr2];
+print_r($combined);
+var_dump($combined);
+
+?>
